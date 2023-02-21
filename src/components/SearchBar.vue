5cb69847fd243ce12441db2b4586b082dd1d7ddd
Jaakko Martikainen <jaakko@martikainen.tech>
2023-02-21 15:22:21 +0100

    added search, new favicon, contact page

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,55 @@
+<script>
+  export default {
+    created() {
+      fetch('api.json')
+        .then((response) => response.json())
+        .then((result) => {
+          this.products = result.data
+        })
+    },
+    data() {
+      return {
+        products: null,
+        search: ''
+      }
+    },
+    computed: {
+      searchResult() {
+        if (this.search) {
+          return this.products.filter((item) => {
+            return this.search
+              .toLowerCase()
+              .split(' ')
+              .every((v) => item.description.toLowerCase().includes(v))
+          })
+        } else {
+          return this.products
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .dropdown li,
+  .dropdown a {
+    background-color: #efded4;
+    width: 100%;
+  }
+
+  .dropdown {
+    position: absolute;
+    padding: 10px;
+  }
+</style>
+
+<template>
+  <input type="text" v-model="search" placeholder="Search products" />
+  <div>
+    <ul v-if="search" class="dropdown">
+      <li v-for="item in searchResult" :key="item.id">
+        <a href="this.item.name"> {{ item.description }} </a>
+      </li>
+    </ul>
+  </div>
+</template>
