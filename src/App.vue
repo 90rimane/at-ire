bf1ff2aeb9504babe2ea85a6bc591d9421e79646
Andrea Högberg <andreahogberg@live.se>
2023-02-23 13:38:42 +0100

    Changed order in App.vue

--- src/App.vue
+++ src/App.vue
@@ -45,10 +45,9 @@
     href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
     rel="stylesheet"
   />
-
+  <HeaderHead />
   <CarouselSlide />
   <SearchBar />
-  <HeaderHead />
   <CitiesList />
   <LandingView />
 </template>
