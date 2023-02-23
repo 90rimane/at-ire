09b206a5e264c50f8ba9d81f9a4d40260fb76e77
Andrea Högberg <andreahogberg@live.se>
2023-02-23 09:20:56 +0100

    Added a header in desktop mode, under progress

--- src/components/HeaderHead.vue
+++ src/components/HeaderHead.vue
@@ -3,31 +3,46 @@
 </script>
 
 <template>
-  <div class="header">
+  <div class="header-mobile">
     <h3>@IRE</h3>
-    <svg
+    <!-- <svg
       xmlns="http://www.w3.org/2000/svg"
       width="16"
       height="16"
       fill="currentColor"
-      class="bi-bi-filter-left"
+      class="bi-bi-filter-left-mobile"
       viewBox="0 0 16 16"
     >
       <path
         d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
       />
-    </svg>
+    </svg> -->
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="16"
       height="16"
       fill="currentColor"
-      class="bi-bi-heart-fill"
+      class="bi-bi-heart-mobile"
       viewBox="0 0 16 16"
     >
       <path
-        fill-rule="evenodd"
-        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
+      />
+    </svg>
+    <span class="material-symbols-outlined">shopping_cart_checkout</span>
+  </div>
+  <div class="header-desktop">
+    <h2>@IRE</h2>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi-bi-search"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
       />
     </svg>
     <svg
@@ -35,86 +50,170 @@
       width="16"
       height="16"
       fill="currentColor"
-      class="bi-bi-cart-fill"
+      class="bi-bi-person-desktop"
       viewBox="0 0 16 16"
     >
       <path
-        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
       />
     </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi-bi-heart-desktop"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
+      />
+    </svg>
+    <span class="material-symbols-outlined">shopping_cart_checkout</span>
   </div>
 </template>
 
 <style scoped>
+  /* @media Temporary added by Ali */
+  @media (max-width: 370px) {
+    .header-mobile {
+      position: absolute;
+      width: 360px;
+      height: 56px;
+      left: 0px;
+      top: 0px;
+      background: #ecc8b2;
+      margin-left: 60px;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
 
-/* @media Temporary added by Ali */
-@media (max-width: 370px) {
-  .header {
-    position: absolute;
-    width: 360px;
-    height: 56px;
-    left: 0px;
-    top: 0px;
-    background: #ecc8b2;
-    margin-left: 30px;
-  }
+    h3 {
+      position: absolute;
+      width: 62px;
+      height: 28px;
+      left: 150px;
+      top: 14px;
+      font-family: 'Karla', roboto, sans-serif;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 24px;
+      line-height: 28px;
+      background: #ecc8b2;
+      color: #000000;
+    }
 
-  h3 {
-    position: absolute;
-    width: 62px;
-    height: 28px;
-    left: 150px;
-    top: 14px;
-    font-family: 'Karla', roboto, sans-serif;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 24px;
-    line-height: 28px;
-    background: #ecc8b2;
-    color: #000000;
-  }
+    /* .bi-bi-filter-left-mobile {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      left: 3.33%;
+      right: 90%;
+      top: 14px;
+      bottom: 95.25%;
+      background: #ecc8b2;
+      color: #000000;
+      cursor: pointer;
+    } */
 
-  .bi-bi-filter-left {
-    position: absolute;
-    width: 30px;
-    height: 30px;
-    left: 3.33%;
-    right: 90%;
-    top: 14px;
-    bottom: 95.25%;
-    background: #ecc8b2;
-    color: #000000;
-    cursor: pointer;
-  }
-
-  /* LÄGG IN EN "NAV-LINK" I APP.VUE
+    /* LÄGG IN EN "NAV-LINK" I APP.VUE
   .bi-bi-list :hover:focus:active {
     background-color: antiquewhite;
   } */
 
-  .bi-bi-heart-fill {
-    position: absolute;
-    width: 20px;
-    height: 20px;
-    left: 81.11%;
-    right: 13.33%;
-    top: 14px;
-    bottom: 95.75%;
-    background: #ecc8b2;
-    color: #000000;
-    cursor: pointer;
+    .bi-bi-heart-mobile {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      left: 81.11%;
+      right: 13.33%;
+      top: 14px;
+      bottom: 95.75%;
+      background: #ecc8b2;
+      color: #000000;
+      cursor: pointer;
+    }
+
+    .material-symbols-outlined {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      left: 329px;
+      top: 14px;
+      background: #ecc8b2;
+      color: #000000;
+      cursor: pointer;
+    }
   }
 
-  .bi-bi-cart-fill {
-    position: absolute;
-    width: 20px;
-    height: 20px;
-    left: 329px;
-    top: 14px;
-    background: #ecc8b2;
-    color: #000000;
-    cursor: pointer;
-  }
+  @media (min-width: 371px) {
+    .header-desktop {
+      box-sizing: border-box;
 
-}
+      position: absolute;
+      width: 1440px;
+      height: 56px;
+      left: 0px;
+      top: 0px;
+
+      background: #ecc8b2;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    h2 {
+      position: absolute;
+      width: 62px;
+      height: 28px;
+      left: 683px;
+      top: 15px;
+
+      font-family: 'Karla';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 24px;
+      line-height: 28px;
+      /* identical to box height */
+
+      color: #000000;
+    }
+
+    /* LÄGG IN EN "NAV-LINK" I APP.VUE
+  .bi-bi-list :hover:focus:active {
+    background-color: antiquewhite;
+  } */
+
+    .bi-bi-search {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      left: 1014px;
+      top: 13px;
+      cursor: pointer;
+    }
+
+    .bi-bi-person-desktop {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      left: 1108px;
+      top: 13px;
+      cursor: pointer;
+    }
+    .bi-bi-heart-desktop {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      left: 1202px;
+      top: 13px;
+      cursor: pointer;
+    }
+
+    .material-symbols-outlined {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      top: 13px;
+      left: 1296px;
+      cursor: pointer;
+    }
+  }
 </style>
