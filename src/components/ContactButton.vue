9ffde68b65e8823e1b8181261eb190f6c0a14bec
Aurorien <starlightnoise@gmail.com>
2023-02-24 17:12:57 +0100

    Add media queries to ContactButton

--- src/components/ContactButton.vue
+++ src/components/ContactButton.vue
@@ -7,10 +7,15 @@
 <style scooped>
   #contact-icon {
     position: fixed;
+    top: 92vh;
+    right: 5vw;
+    /* top: 700px;
+    right: 75px; */
+    /* bottom: auto; */
     z-index: 1;
-    margin-left: 100px;
-    margin-top: 87vh;
-    margin-left: 90vw;
+    /* margin-left: 100px; */
+    /* margin-top: 90vh;
+    margin-left: 83vw; */
     height: 42px;
     width: 47px;
     font-size: 50px;
@@ -33,5 +38,45 @@
     box-shadow: 0px 0.2px 1.9px #f39256, 0px 0.5px 4.5px #f39256,
       0px 0.8px 8.5px #f39256, 0px 1.4px 15.2px #f39256,
       0px 2.4px 28.4px #f39256, 0px 5px 68px #f39256;
+  }
+
+  /* media queries mainly for flipped mobile */
+  @media (min-width: 650px) and (max-width: 979px) {
+    #contact-icon {
+      top: 80vh;
+      right: 5vw;
+    }
+  }
+
+  /* media queries for deskop */
+  @media (min-width: 980px) {
+    #contact-icon {
+      top: 90vh;
+      right: 5vw;
+    }
+  }
+
+  /* media queries for smaller squarer phones  */
+  @media (max-width: 380px) and (max-height: 670px) {
+    #contact-icon {
+      top: 89vh;
+      right: 7vw;
+    }
+  }
+
+  /* media queries for tablet */
+  @media (min-width: 809px) and (max-height: 1089px) {
+    #contact-icon {
+      top: 92vh;
+      right: 7vw;
+    }
+  }
+
+  /* media queries for  big widescreen tablet */
+  @media (min-width: 799px) and (max-height: 1300px) {
+    #contact-icon {
+      top: 90vh;
+      right: 4vw;
+    }
   }
 </style>
