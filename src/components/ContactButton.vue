2ffb241b0ee24b12ec6974f83be12240250229f5
Aurorien <starlightnoise@gmail.com>
2023-02-24 10:49:55 +0100

    Add styling and remove second layer as backgound on ContactButton

--- src/components/ContactButton.vue
+++ src/components/ContactButton.vue
@@ -1,30 +1,32 @@
 <template>
   <div id="contact-icon" class="material-symbols-outlined">contact_support</div>
-  <div id="background-icon" />
 </template>
 
 <style scooped>
-  #contact-icon,
-  #background-icon {
-    margin-left: 100px;
-    font-size: 50px;
-    margin-top: 87vh;
-    margin-left: 90vw;
+  #contact-icon {
     position: fixed;
     z-index: 1;
-  }
-
-  #background-icon {
+    margin-left: 100px;
+    margin-top: 87vh;
+    margin-left: 90vw;
+    height: 42px;
+    width: 47px;
+    font-size: 50px;
     background-color: #ecc8b2;
-    z-index: initial;
-    height: 30px;
-    width: 30px;
-    margin-top: 88vh;
-    margin-left: 91vw;
     border-radius: 50%;
     /* box-shadow with a little help from a nice tool: https://shadows.brumm.af/ */
     box-shadow: 0px 0.2px 1.9px #ecc8b2, 0px 0.5px 4.5px #ecc8b2,
       0px 0.8px 8.5px #ecc8b2, 0px 1.4px 15.2px #ecc8b2,
       0px 2.4px 28.4px #ecc8b2, 0px 5px 68px#ecc8b2;
+  }
+
+  #contact-icon:hover {
+    background-color: #f39256;
+    height: 55px;
+    width: 57px;
+    font-size: 60px;
+    box-shadow: 0px 0.2px 1.9px #f39256, 0px 0.5px 4.5px #f39256,
+      0px 0.8px 8.5px #f39256, 0px 1.4px 15.2px #f39256,
+      0px 2.4px 28.4px #f39256, 0px 5px 68px #f39256;
   }
 </style>
