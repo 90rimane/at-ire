e7cd6fa741e8d0a91c06c634fbe80fc832805f57
Jovan <jovan.14@hotmail.com>
2023-02-27 12:20:07 +0100

    Moved links to index.html

--- src/App.vue
+++ src/App.vue
@@ -39,19 +39,6 @@
     <RouterView />
   </div>
 
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap"
-    rel="stylesheet"
-  />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
-    rel="stylesheet"
-  />
-
   <HeaderHead />
 
   <FooterComponent v-if="isMobile" />
