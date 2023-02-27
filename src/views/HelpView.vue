f0857a7bbf7aa4b4233a1e7693fa156ac17aab55
90rimane <rima.aneta@gmail.com>
2023-02-27 15:32:48 +0100

    new packaged adde, and primary chat created

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,6 +1,189 @@
+<script setup>
+import AgoraRTM from 'agora-rtm-sdk';
+import { v4 as uuidv4 } from 'uuid';
+import { ref, onMounted, nextTick, defineExpose } from 'vue';
+
+
+const APP_ID = '452f99a0814b44d29d9a446ec20356fc';
+// const APP_ID = '766b26b8564f4d22a1c6cc908547fd8f';
+const CHANNEL = 'atire';
+let client = AgoraRTM.createInstance(APP_ID);
+let uid = uuidv4();
+let text = ref('');
+let messagesRef = ref(null);
+let messages = ref([]);
+let channel;
+
+defineExpose({ messagesRef });
+
+const appendMessage = async (message) => {
+  messages.value.push(message);
+  await nextTick();
+  messagesRef.value.scrollTop =
+    messagesRef.value.scrollHeight;
+};
+
+onMounted(async () => {
+  await client.login({ uid, token: null });
+  channel = await client.createChannel(CHANNEL);
+  await channel.join();
+  channel.on('ChannelMessage', (message, peerId) => {
+    appendMessage({
+      text: message.text,
+      uid: peerId,
+    });
+  });
+});
+
+function sendMessage() {
+  if (text.value === '') return;
+  channel.sendMessage({ text: text.value, type: 'text' });
+  appendMessage({
+    text: 'YOU:'+' '+ text.value,
+    uid,
+  });
+  text.value = '';
+}
+</script>
+
 <template>
-  <main class="home-pages">
-    <h1>Help page</h1>
-    <p>There is online support/chat</p>
+  <main>
+    <div class="panel">
+      <h3>
+        <span class="material-symbols-outlined support-logo">support_agent</span>
+        Online support
+      </h3>
+      <div class="messages" ref="messagesRef">
+        <div class="inner">
+          <div
+            :key="index"
+            v-for="(message, index) in messages"
+            class="message"
+          >
+            <div v-if="message.uid === uid" class="user-self">
+              &nbsp;
+            </div>
+            <div v-else class="user-them">_&nbsp;</div>
+            <div class="text">{{ message.text }}</div>
+          </div>
+        </div>
+      </div>
+
+      <form @submit.prevent="sendMessage">
+        <input v-model="text" />
+        <button>
+          <span class="material-symbols-outlined send-msg">send</span>
+        </button>
+      </form>
+    </div>
   </main>
   </template>
+
+  <style lang="scss" scoped>
+
+  main {
+    padding: 2em;
+    height: 100%;
+    text-align: center;
+    background: linear-gradient(
+      90deg,
+      var(--light) 0%,
+      var(--lightB) 53%,
+      var(--lightB-darker) 100%
+    );
+  }
+
+  .panel {
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+    margin: 12em auto;
+    max-width: 25em;
+    height: 30em;
+    background: var(--lightB-darker);
+    box-shadow: 0 5px 20px 0 var(--darker);
+    backdrop-filter: blur(4px);
+    border-radius: 10px;
+    h3{
+      padding-bottom: 5px;
+    }
+  }
+  .messages {
+    height: 100%;
+    width: 100%;
+    overflow-y: scroll;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    background-color: var(--light);
+  }
+  .inner {
+    padding: 10px;
+  }
+  .message {
+    font-size: 18px;
+    text-align: left;
+    display: flex;
+    margin-bottom: 6px;
+  }
+  .user-self {
+    color: green;
+  }
+  .user-them {
+    color: red;
+  }
+  form {
+    position: relative;
+    display: flex;
+    align-content: center;
+  }
+  input {
+    width: 100%;
+    border: none;
+    height: 40px;
+    padding: 8px;
+    border-top: 1px solid #999;
+    border-radius: 0px;
+    outline: none;
+    font-size: 18px;
+    background-color: var(--lightB);
+  }
+  button {
+    border: none;
+    outline: none;
+    position: absolute;
+    right: 5px;
+    top: 4px;
+  }
+  .send-msg{
+    background-color: var(--orange);
+    font-size: 2.3em;
+    padding-left: 4px;
+    padding-top: 2px;
+    width: 35px;
+    height: 35px;
+    border-radius: 30px;
+  }
+  @media (max-width: 600px) {
+    .panel {
+    padding: 1em;
+    margin: 5em auto;
+    max-width: 20em;
+    height: 30em;
+    background: var(--lightB-darker);
+    box-shadow: 0 5px 20px 0 var(--darker);
+    backdrop-filter: blur(4px);
+    border-radius: 10px;
+  }
+  @media (max-width: 470px){
+    .panel{
+      margin: 5em -2em 7em 2em;
+    }
+    .message {
+    font-size: 14px;
+    }
+    input{
+      font-size: 14px;
+    }
+  }
+}
+  </style>
