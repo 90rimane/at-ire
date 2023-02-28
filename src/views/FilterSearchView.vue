d7a27e07d6a2af6357cdc8d78d9744537bb3ad08
Jovan <jovan.14@hotmail.com>
2023-02-28 10:35:26 +0100

    Mapped "quantity" property to allProducts

---
+++ src/views/FilterSearchView.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<template>
+  <div>hello</div>
+</template>
+
+<style scoped lang="scss"></style>
