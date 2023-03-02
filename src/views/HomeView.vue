c149332ac33b3a9b4301b6221de2f3b4f82cd96f
Jovan <jovan.14@hotmail.com>
2023-03-02 15:27:53 +0100

    Updated HomeView with router-links for search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,38 +7,39 @@
       <img
         src="https://i.pinimg.com/564x/a8/c1/55/a8c1552fc6e272a18faba1dc01fbf0f9.jpg"
       />
-      <div class="text">TOPS</div>
+      <router-link to="/search/Tops" class="text">TOPS</router-link>
     </div>
 
     <div class="box">
       <img
         src="https://i.pinimg.com/564x/f2/e8/13/f2e8134dac23fcb325f4772840a2a1d4.jpg"
       />
-      <div class="text">BOTTOMS</div>
+      <router-link to="/search/Bottoms" class="text">BOTTOMS</router-link>
     </div>
 
     <div class="box">
       <img
         src="https://images.unsplash.com/photo-1606760227091-3dd870d97f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
       />
-      <div class="text">ACCESSORIES</div>
+      <router-link to="/search/Accessories" class="text"
+        >ACCESSORIES</router-link
+      >
     </div>
 
     <div class="box">
       <img
         src="https://i.pinimg.com/564x/43/c2/a5/43c2a55207dd738d37dd3db5edffcc55.jpg"
       />
-      <div class="text">PANTS</div>
+      <router-link to="/search/Pants" class="text">PANTS</router-link>
     </div>
   </div>
 
-<TrendingSection />
-
+  <TrendingSection />
 </template>
 
 <script>
   import CarouselSlide from '../components/CarouselSlide.vue'
-  import TrendingSection from '../components/TrendingSection.vue';
+  import TrendingSection from '../components/TrendingSection.vue'
   export default {
     components: {
       CarouselSlide,
@@ -49,10 +50,7 @@
         return this.$store.state.allProducts
       }
     }
-
   }
-
-
 </script>
 
 <style scoped>
@@ -61,7 +59,7 @@
     flex-wrap: wrap;
     gap: 20px;
     margin-top: 80px;
-    padding-bottom:100px;
+    padding-bottom: 100px;
   }
 
   .box {
@@ -91,7 +89,4 @@
     text-align: center;
     text-shadow: 2px 2px 4px #000000;
   }
-
-
-
 </style>
