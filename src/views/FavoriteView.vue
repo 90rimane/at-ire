dec539b155af263451e0dc719146dbfd8ae9664f
Jovan <jovan.14@hotmail.com>
2023-03-02 20:29:54 +0100

    Styled favorite site

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -5,13 +5,6 @@
       productItem
     },
     computed: {
-      testproducts() {
-        // slice method to only display 4 items at once
-        if (this.$store.state.allProducts) {
-          return this.$store.state.allProducts.slice(0, 1)
-        }
-        return null
-      },
       getfavorites() {
         // slice method to only display 4 items at once
         if (this.$store.state.activeUser) {
@@ -25,26 +18,47 @@
 
 <template>
   <main class="home-pages">
-    <h1>Favorite</h1>
-    <p>Make list of favorites here</p>
-    <h2>products</h2>
-    <div v-if="testproducts != null" class="all-items">
-      <productItem
-        v-for="product in testproducts"
-        :key="product.id"
-        :product="product"
-      />
-    </div>
-    <h2>Favorites</h2>
-    <div v-if="testproducts != null" class="all-items">
+    <h2>My Favorites</h2>
+    <div v-if="$store.state.activeUser != null" class="favorite-display">
       <productItem
         v-for="product in getfavorites"
         :key="product.id"
         :product="product"
       />
     </div>
-    <router-link to="/search" class="button">
-      <div style="width: 50px; height: 50px; background-color: red" />
-    </router-link>
   </main>
 </template>
+
+<style scoped lang="scss">
+  @media only screen and (min-width: 980px) {
+    main {
+      margin-left: 68px;
+    }
+  }
+  main {
+    padding-top: 80px;
+
+    h2 {
+      margin-left: 40px;
+    }
+
+    .favorite-display {
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+      align-items: center;
+      padding: 20px 0 60px 0;
+      max-width: 990px;
+      margin: auto;
+    }
+
+    @media only screen and (min-width: 690px) {
+      .favorite-display {
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+</style>
