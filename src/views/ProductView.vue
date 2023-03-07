7c2c7ae1df98b58fb7f2551bd431d0e1faaef8da
Jovan <jovan.14@hotmail.com>
2023-03-07 17:08:11 +0100

    Finished changes to cartView, modifed a method in productView, removed mapped quantity amount in SET_PRODUCTS mutation

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -2,7 +2,7 @@
   <article id="article" v-if="oneProduct">
     <header>
       <!-- spans to be made to breadcrumb-navigation  -->
-      <span>{{ this.oneProduct.category }} --> </span>
+      <span>{{ this.oneProduct.category }} </span>
       <span>{{ this.oneProduct.subcategory }}</span>
       <h1>{{ this.oneProduct.description }}</h1>
       <img
@@ -49,7 +49,7 @@
     <section id="price-cart">
       <h2 id="price">Price: {{ this.oneProduct.price }}:-</h2>
       <!-- Routerlink added by Andrea -->
-      <router-link to="/checkout">
+      <router-link to="/cart">
         <button id="cart-button" @click="onAddToCart" :disabled="!bothSelected">
           Add to cart
           <span id="cart-icon" class="material-symbols-outlined"
@@ -62,6 +62,8 @@
 </template>
 
 <script>
+  import { v4 as uuidv4 } from 'uuid'
+
   export default {
     computed: {
       oneProduct() {
@@ -83,13 +85,18 @@
     methods: {
       onAddToCart() {
         // to make an object with the specified color and size -->
+        // Changed by Jovan: Instead of spreading out the whole array I've resorted
+        // to only adding the parts needed for the cart to display, additionlly
+        // added a unique product id with uuid
         this.cartProduct = {
-          ...this.oneProduct,
+          id: uuidv4(),
+          name: this.oneProduct.name,
+          description: this.oneProduct.description,
+          price: this.oneProduct.price,
           colors: this.selectedColor,
           img: this.selectedColorImg,
           sizes: this.selectedSize
         }
-        console.log('cartProduct', this.cartProduct)
 
         this.activeUser = JSON.parse(sessionStorage.getItem('activeUser'))
         this.activeUser.cart.push(this.cartProduct)
