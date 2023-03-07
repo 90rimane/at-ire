dd5636c808dfabdf1a68a646705b2933f60cec12
Aurorien <starlightnoise@gmail.com>
2023-03-07 12:22:03 +0100

    Add hover-connection between search-icon and search-inputfield in HeaderHead.vue and refined that a bit.

--- src/components/HeaderHead.vue
+++ src/components/HeaderHead.vue
@@ -209,11 +209,31 @@
     background-color: hsla(0, 0%, 23%, 0.3);
   }
 
+  /* add by Anna --> */
+
+  form {
+    position: relative;
+  }
+
+  form:hover .search-bar-mobile {
+    width: 174px;
+    cursor: pointer;
+    background: #fff5ef;
+    z-index: 20;
+  }
+
+  .search-bar-mobile:focus {
+    background: #fff5ef;
+    width: 174px;
+  }
+
+  /* <-- add by Anna + some changes related to this around here*/
+
   .bi-bi-search-mobile {
     position: absolute;
     width: 30px;
     height: 30px;
-    left: 41%;
+    left: 44%;
     top: 13px;
     cursor: pointer;
     color: #000000;
@@ -221,26 +241,26 @@
 
   .search-bar-mobile {
     position: fixed;
-    width: 59px;
+    width: 1px;
     height: 40px;
     left: 49%;
     top: 8px;
     background: #ecc8b2;
     border-radius: 13px;
     border: 1px;
-    padding-left: 8px;
+    padding-left: 0.8em;
     font-size: 17px;
     transition: 0.9s;
     outline: none;
     display: inline-block;
   }
 
-  .search-bar-mobile:hover {
+  /* .search-bar-mobile:hover {
     width: 174px;
     cursor: pointer;
     background: #fff5ef;
     z-index: 20;
-  }
+  } */
 
   h3 {
     position: absolute;
