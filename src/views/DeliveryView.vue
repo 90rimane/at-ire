db3697f7f48a7943901791284fd55d0e43a50b92
Jaakko Martikainen <jaakko@martikainen.tech>
2023-03-13 15:19:27 +0100

    small fix

--- src/views/DeliveryView.vue
+++ src/views/DeliveryView.vue
@@ -2,7 +2,7 @@
   <main>
     <h1>Delivery</h1>
     <h2>Choose your delivery method:</h2>
-    <p v-if="user.delivery">
+    <p v-if="user">
       ( ps. last time you chose {{ user.delivery[0] }} {{ user.delivery[1] }})
     </p>
 
