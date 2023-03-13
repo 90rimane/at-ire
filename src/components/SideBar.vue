ad78ac657f9c1ca437b319e01272c44e1853f39a
Aurorien <starlightnoise@gmail.com>
2023-03-13 14:55:45 +0100

    Add comments

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -24,9 +24,7 @@
         <span class="material-symbols-outlined">home</span>
         <span class="text">Home</span>
       </router-link>
-      <!-- both color and hover color of the icon change via props when hover the cart menu bar, in that way the icon practically sync color with the menu bar text
-              @mouseover="hovCol = '#64748b'"
-        @mouseleave="hovCol = '#1e293b'"-->
+      <!-- both color and hover color of the icon change via props when hover the cart menu bar, in that way the icon practically sync color with the menu bar text-->
       <router-link
         to="/cart"
         class="button"
@@ -105,9 +103,12 @@
 
   let hovCol = ref('')
   const ToggleCartIconColor = (bool) => {
+    // first checks if router-link is active, if so the icons color is simply set to --dark without hover color change
     if (bool === 'active' && (bool === true || bool === false)) {
       hovCol.value = 'var(--dark)'
     } else if (bool === 'inactive' && (bool === true || bool === false)) {
+      // if it's not active these conditions runs and the hover colors will show
+      // timeout is set to match the render-time of the styling of the Shopping Cart text
       if (bool === true) {
         setTimeout(() => {
           hovCol.value = 'var(--grey)'
