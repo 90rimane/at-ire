356255086655ed29d819f04f1c52a3d6c135f60c
90rimane <rima.aneta@gmail.com>
2023-03-18 14:24:55 +0100

    some changes added

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // build: {
-  //   rollupOptions: {
-  //     external: ['vue', 'vite']
-  //   }
-  // }
+  build: {
+    chunkSizeWarningLimit: 1200
+    // rollupOptions: {
+    //   output:{
+    //     manualChunks(id) {
+    //         if (id.includes('node_modules')) {
+    //             return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //         }
+    //     }
+    //   }
+    // }
+  }
 })
