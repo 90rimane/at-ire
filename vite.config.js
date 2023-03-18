4ad577ede321c60f0c5944183afa3d6781a4539c
90rimane <rima.aneta@gmail.com>
2023-03-18 13:53:22 +0100

    working on footer to run build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.html')
-        }
-      }
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['vue', 'vite']
+  //   }
+  // }
 })
